37ad9d7f7057b4afa537aa5cc6935a7c7f9bb00e
mehedihasanmehedi009 <mehedihasanmehedi007008@gmail.com>
2025-10-22 22:39:11 -0600

    mehedi

--- src/Pages/Doctor.jsx
+++ src/Pages/Doctor.jsx
@@ -5,7 +5,7 @@ const Doctor = () => {
   const Catagory = use(doctor);
   return (
     <div>
-      <h1 className="text-5xl font-extrabold bg-gradient-to-r from-red-500 to-blue-500 text-transparent bg-clip-text">
+      <h1 className="text-5xl font-extrabold bg-gradient-to-r from-red-500 to-blue-500 text-transparent bg-clip-text text-center p-5">
         Health Care Provider
       </h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1   p-4 gap-5 ">
